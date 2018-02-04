fcebd23adb8cd00a74fdb48be993893eb89cf683
Chuck Reynolds <chuck@vuurr.com>
2018-02-03 19:26:16 -0700

    reinstate index nav, update more link

--- index.php
+++ index.php
@@ -7,21 +7,20 @@
 
 			<h1 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h1>
 			<div class="entry">
-				<?php the_content('[Click to Continue...]'); ?>
+				<?php the_content('[Click to Continue Reading...]'); ?>
                 <p class="postMeta">Posted <span class="postMetaInfo"><?php the_time(get_option('date_format')); ?></span> in <span class="postMetaInfo"><?php the_category(', ') ?></span> | <?php comments_popup_link('Be First to Comment', '1 Comment', '% Comments'); ?></p>
 			</div><!-- END entry -->
-
-
 
 			<div class="clrBoth"></div>
 
 			<?php endwhile; ?>
 
-<?php /*			<div class="navigation">
-				<div class="alignleft"><?php next_posts_link('&laquo; Previous Entries') ?></div>
-				<div class="alignright"><?php previous_posts_link('Next Entries &raquo;') ?></div>
+
+			<div class="navigation">
+				<div class="alignleft"><?php next_posts_link('&laquo; Older Posts') ?></div>
+				<div class="alignright"><?php previous_posts_link('Newer Posts &raquo;') ?></div>
 			</div>
-*/ ?><?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } ?>
+
 
 		<?php else : ?>
 
